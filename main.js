513425a21156078596632756446d5809bc246d5d
simon <simonlhopkins@gmail.com>
2020-04-13 13:01:37 -0400

    hehe

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   console.log('hello');
   $("a").draggable();
+  $("p").draggable();
   height = $("#mainImg").width();
   $("#asLink").css("top", height/4.3);
   $("#asLink").css("left", height/2.67);
