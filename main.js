9e0ee2154f5692cbed9a57f9559f114f0dd687d4
simon <simonlhopkins@gmail.com>
2020-04-13 12:57:54 -0400

    yeet

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   console.log('hello');
+  $("a").draggable();
   height = $("#mainImg").width();
   $("#asLink").css("top", height/4.3);
   $("#asLink").css("left", height/2.67);
